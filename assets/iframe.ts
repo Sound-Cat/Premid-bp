02188c84b3820f21759cae31a045811820ecdb14
Sound-Cat <76255766+Sound-Cat@users.noreply.github.com>
2021-04-17 18:12:32 +0200

    Created asset files

---
+++ assets/iframe.ts
@@ -0,0 +1,12 @@
+const iframe = new iFrame();
+iframe.on("UpdateData", async () => {
+  /*
+  Get all the data you need out of the iFrame save them in variables
+  and then sent them using iframe.send
+  */
+  iframe.send({
+    //sending data
+    video: video,
+    time: video.duration
+  });
+});
